ea4a122fe4d202d5c55a51d3000b312f5465c73d
KarimB101 <karim.baptiste@yahoo.com>
2023-05-05 15:19:45 -0500

    start with chat

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const Category = require('../models/Category');
+
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
+});
+router.get('/', async (req, res) => {
+  const categories = await Category.findAll();
+  res.json(categories);
 });
 
 module.exports = router;
