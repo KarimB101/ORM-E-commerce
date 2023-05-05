8404eb10557873819200949aa16f356421eb7af1
KarimB101 <karim.baptiste@yahoo.com>
2023-05-05 14:10:33 -0700

    made models

--- models/Category.js
+++ models/Category.js
@@ -1,15 +1,21 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
+
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-    name: {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    category_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     }
   },
   {
@@ -20,5 +26,4 @@ Category.init(
     modelName: 'category',
   }
 );
-
 module.exports = Category;
--- models/Tag.js
+++ models/Tag.js
@@ -1,14 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
-  },
-  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    tag_name: {
+      type: DataTypes.STRING
+    }
+  },  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
