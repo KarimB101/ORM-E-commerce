9866921b9973c287eac5baeb9a8d7a4728ab4a02
KarimB101 <karim.baptiste@yahoo.com>
2023-05-05 15:29:23 -0500

    edit connection to sql

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,16 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const Category = require('../models/Category');
 
-
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
 router.get('/', async (req, res) => {
+  console.log("get route hit")
   const categories = await Category.findAll();
   res.json(categories);
 });
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>")
+});
+
 
 module.exports = router;
